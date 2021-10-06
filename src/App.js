a5b9eb6401d98dfb62f5f39422625e77634bfadc
Farouk Goaudria <gouadriaafar@gmail.com>
2021-10-07 00:44:20 +0100

    navbar style

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import Nav from './components/Nav';
+import {BrowserRouter as Router,Route,Switch} from "react-router-dom";
+import Home from './pages/Home';
+import About from './pages/About';
+import Projects from './pages/Projects';
+import Contact from './pages/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+      <Router>
+          <Nav />
+          <Switch>
+            <Route path="/about">
+                <About />
+            </Route>
+            <Route path='/projects'>
+                <Projects />
+            </Route>
+            <Route path="/contact">
+                <Contact />
+            </Route>
+            <Route path="/">
+                  <Home />
+            </Route>
+          </Switch>
+      </Router>
+          
+      </>
   );
 }
 
